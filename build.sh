00341b73cf9e6c7a82094780dfd5be8c29a03d2d
xzt <qq87315156@gmail.com>
2022-09-09 10:04:35 +0800

    refine

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
-#!/bin/sh
+#!/usr/bin/sh   
 
-branch="dev"
 #进程关键字
 processName="insurance-after"
 #项目启动命令
 projectCmd=target/${processName}-1.0-SNAPSHOT.jar
 startCmd="java -jar $projectCmd"
-#进去fish
-#fish
-
 #拉取代码
 git checkout .
-git pull origin ${branch}
-expect "appUsername for"
-send "cd xiaozhongting\r"
-expect "Password for"
-send "cd zx27892509\r"
+echo "------- 拉取代码 ---------"
+expect <<EOF  
+spawn git pull
+expect "Username*"
+send "xiaozhongting\r"
+expect "Password for*"
+send "zx27892509\r"
+expect eof
+EOF
 
 #编译
 if mvn package; then
@@ -31,6 +31,6 @@ processId=$(ps aux | grep ${processName}  |awk '{print $2}' | tr "\n" " ")
 echo $processId
 kill -9 ${processId}
 
-# 后台启动项目
+# # 后台启动项目
 echo $startCmd
 nohup $startCmd &
